055cc1b57edb0e9b427abd4ec11d5422379b1b17
Shelton <dellashelton@quickenloans.com>
2020-03-09 11:24:21 -0400

    Dice class added

---
+++ RPGPart2/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGPart2
+{
+    public class Dice
+    {
+        Random rand = new Random();
+        public int D20()
+        {
+            return rand.Next(1, 21);
+        }
+
+
+        public int D8()
+        {
+            return rand.Next(1, 9);
+        }
+
+        public int D6()
+        {
+            return rand.Next(1, 7);
+        }
+
+        public int D4()
+        {
+            return rand.Next(1, 5);
+        }
+    }
+}
