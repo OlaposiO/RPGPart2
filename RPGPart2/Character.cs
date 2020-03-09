9eae3ccb9d1d3e1a5d69089a1ee924aae68130d0
Mike Schwartz <mikeschwartz@quickenloans.com>
2020-03-09 13:36:15 -0400

    Udates to classes

--- RPGPart2/Character.cs
+++ RPGPart2/Character.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace RPGPart2
 {
-    class Character
+    abstract class Character
     {
+        public int HitPoints { get; set; }
+        public int Strength { get; set; }
+        public int Dex { get; set; }
+        public int Intelligence { get; set; }
+        public int Armor { get; set; }
+
+        public Character(int hitPoints, int strength, int dex, int intelligence, int armor)
+        {
+            HitPoints = hitPoints;
+            Strength = strength;
+            Dex = dex;
+            Intelligence = intelligence;
+            Armor = armor;
+        }
     }
 }
